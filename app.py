38ce74d7c40d1d0b73ef06a0d349bc2cd86b06f0
smrvaghefi <smr_vaghefi2000@yahoo.com>
2025-04-21 17:15:52 +0330

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from tabpy.tabpy_server.app.app import TabPyApp
 import os
 
+# اضافه کردن لاگ موقت برای بررسی محتویات فایل پسورد
+with open('passwords.txt', 'r') as f:
+    print("=== Password file contents ===")
+    print(f.read())
+
 config_file = os.path.join(os.path.dirname(__file__), 'tabpy.conf')
 app = TabPyApp(config_file=config_file)
 app.run()
