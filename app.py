b88f2a46ae884ba73a7229c78c5c29537bfe2aca
smrvaghefi <smr_vaghefi2000@yahoo.com>
2025-04-22 13:32:38 +0330

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from tabpy.tabpy_server.app.app import TabPyApp
 import os
 
+db_user = os.getenv('DB_USER')
+db_password = os.getenv('DB_PASSWORD')
 config_file = os.path.join(os.path.dirname(__file__), 'tabpy.conf')
 app = TabPyApp(config_file=config_file)
 app.run()
