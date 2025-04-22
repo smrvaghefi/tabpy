c78aa6ffd3b7e380597fb63ef748aa4e63166735
smrvaghefi <smr_vaghefi2000@yahoo.com>
2025-04-22 16:38:17 +0330

    Update app.py

--- app.py
+++ app.py
@@ -4,5 +4,5 @@ import os
 db_user = os.getenv('DB_USER')
 db_password = os.getenv('DB_PASSWORD')
 config_file = os.path.join(os.path.dirname(__file__), 'tabpy.conf')
-app_instance = TabPyApp(config_file=config_file)
-app = app_instance._create_wsgi_app()
+app = TabPyApp(config_file=config_file)
+app.run()
